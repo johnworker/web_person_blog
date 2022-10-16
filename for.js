4680ba5234e94f096c561bd8aad03fa9b906575f
johnworker <78348941+johnworker@users.noreply.github.com>
2022-10-16 22:09:19 +0800

    添加內容

---
+++ for.js
@@ -0,0 +1,15 @@
+// for 迴圈
+
+// var i = 0;
+// while(i<10){
+//     document.write(i);
+//     document.write("<br/>");
+//     i++;
+// }
+
+var friends = ["小黑","小黃","小綠"]
+
+for(var i = 0; i<friends.length; i++){
+    document.write(friends[i]);
+    document.write("<br/>");
+}
